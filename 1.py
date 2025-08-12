3332119a9cf6cac103344fe1079428505187fe8a
bansikumarujeniya <bansikumarujeniya@gmail.com>
2025-08-12 11:27:41 +0530

    1.py and 2.py python files

---
+++ 1.py
@@ -0,0 +1,18 @@
+#1
+#this is a comment
+print("Hello World")
+
+#2
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
+
+
+#3
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
